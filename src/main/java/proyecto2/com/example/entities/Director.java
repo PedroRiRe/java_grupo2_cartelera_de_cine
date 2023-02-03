c43a5358e20444fba90f4b3cb48b075ac5dabf3f
PedroRiRE <enlosojsdepedro@hotmail.com>
2023-02-03 13:08:10 +0100

    Entidades y Repositorios director y cine en desarrollo

--- src/main/java/proyecto2/com/example/entities/Director.java
+++ src/main/java/proyecto2/com/example/entities/Director.java
@@ -2,6 +2,8 @@ package proyecto2.com.example.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "director")
 public class Director {
@@ -9,6 +11,13 @@ public class Director {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    private String name;
+
+    private  String surname;
+
+    private String cityBirth;
+    private String country;
+    private LocalDate dateBirth;
 
     public Long getId() {
         return id;
@@ -18,4 +27,73 @@ public class Director {
         this.id = id;
     }
 
+
+    public Director() {
+    }
+
+    public Director(String name, String surname, String cityBirth, String country, LocalDate dateBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.cityBirth = cityBirth;
+        this.country = country;
+        this.dateBirth = dateBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Director setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Director setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getCityBirth() {
+        return cityBirth;
+    }
+
+    public Director setCityBirth(String cityBirth) {
+        this.cityBirth = cityBirth;
+        return this;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Director setCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public LocalDate getDateBirth() {
+        return dateBirth;
+    }
+
+    public Director setDateBirth(LocalDate dateBirth) {
+        this.dateBirth = dateBirth;
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Director{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", cityBirth='" + cityBirth + '\'' +
+                ", country='" + country + '\'' +
+                ", dateBirth=" + dateBirth +
+                '}';
+    }
 }
