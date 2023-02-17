0b3e3a89d793fd39c57ae86e86613337da1b0456
JavGuerra <javguerra@badared.com>
2023-02-17 10:12:30 +0100

    Revisión de errores y refactorización

--- src/main/java/cartelera/entities/Film.java
+++ src/main/java/cartelera/entities/Film.java
@@ -8,41 +8,52 @@ import lombok.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Setter
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Getter
+@Setter
 @ToString
+@Builder
 @Entity
-@Table(name = "film")
 public class Film {
-    /*
-    crear attributos
-    trailer
-    poster
-    valoración externa
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String title;
+
     private Integer duration;
+
     private String director;
+
     private String country;
+
     private Classification classification;
+
     private Byte rating;
+
     private String music;
+
     private  String photography;
+
     private String script;
+
     private String company;
+
     @ElementCollection
     private Set<String> actors = new HashSet<>();
+
     @ElementCollection
     private Set<Gender> genders;
 
+    private String trailer;
+
+    private String poster;
+
+    private String review;
+
     @OneToMany(mappedBy = "film", fetch = FetchType.EAGER)
-    // @ElementCollection  //¿? Teniendo el mapped by no es necesario
     private Set<Room> rooms = new HashSet<>();
 }
