a1b51dea2b062251c8c7e75e37b3843379a6e21e
Yo231s <dawid.lempicki.1@gmail.com>
2023-02-17 18:44:27 +0100

    primer commit

--- src/main/java/cartelera/repositories/HallRepository.java
+++
@@ -1,8 +0,0 @@
-package cartelera.repositories;
-
-import cartelera.entities.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public abstract class HallRepository implements JpaRepository<Room, Long> {
-
-}
---
+++ src/main/java/cartelera/services/impl/RoomServiceImpl.java
@@ -0,0 +1,36 @@
+package cartelera.services.impl;
+
+import cartelera.entities.Room;
+import cartelera.repositories.RoomRepository;
+import cartelera.services.IRoomService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class RoomServiceImpl implements IRoomService {
+
+        private final RoomRepository roomRepo;
+
+
+    @Override
+        public List<Room> findAll() {
+            log.info("findAll");
+            return roomRepo.findAll();
+        }
+
+        @Override
+        public Optional<Room> findById(Long id) {
+            log.info("findById {}", id);
+            if (id == null || id <= 0) return Optional.empty();
+            return roomRepo.findById(id);
+        }
+    }//
+
+
