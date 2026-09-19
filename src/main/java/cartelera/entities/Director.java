aa83d17443d489aff4bee97601b0b017a503df86
PedroRiRE <enlosojsdepedro@hotmail.com>
2026-09-19 14:28:46 +0200

    feat(entities): añade entidad Director corregida y preparada para relaciones

--- src/main/java/cartelera/entities/Director.java
+++ src/main/java/cartelera/entities/Director.java
@@ -2,6 +2,8 @@ package cartelera.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,19 +12,22 @@ import lombok.*;
 @Builder
 @Entity
 public class Director {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private String document;
+    @Id
+    private String document; // Clave primaria de tipo texto (ej. DNI/Pasaporte) sin autoincremento
 
     private String name;
 
     private String surname;
 
-    private String movies;
-
     private String image;
 
+    // Relación ManyToMany recomendada con Film para aprovechar JPA
+    @ManyToMany(mappedBy = "directors")
+    private Set<Film> films = new HashSet<>();
 
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
 }
-
